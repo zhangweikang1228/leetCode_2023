bbfc2311bcb78ad5d20c32d089e8199251875a9d
zhangweikang1228 <565927608@qq.com>
2023-02-13 16:25:24 +0000

    leetCode 24.两两交换链表中的节点 19.删除链表的倒数第 N 个结点 160.链表相交 142.环形链表

--- leetcode/src/main/java/com/kang/linkList/LinkListSolution.java
+++ leetcode/src/main/java/com/kang/linkList/LinkListSolution.java
@@ -65,6 +65,7 @@ public class LinkListSolution {
      * 203. Remove Linked List Elements
      * Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val,
      * and return the new head.
+     *
      * @param head
      * @param val
      * @return
@@ -84,7 +85,7 @@ public class LinkListSolution {
         return fakeHead.next;
     }
 
-    public static ListNode removeElements2(ListNode head, int val){
+    public static ListNode removeElements2(ListNode head, int val) {
         while (head != null && head.val == val) {
             head = head.next;
         }
@@ -110,6 +111,7 @@ public class LinkListSolution {
     /**
      * 206. Reverse Linked List
      * Given the head of a singly linked list, reverse the list, and return the reversed list.
+     *
      * @param head
      * @return
      */
@@ -128,6 +130,7 @@ public class LinkListSolution {
 
     /**
      * 递归来写206.反转链表
+     *
      * @param head
      * @return
      */
@@ -147,5 +150,162 @@ public class LinkListSolution {
         // cur = temp;
         return reverse(cur, temp);
     }
+
+
+    /**
+     * 24. Swap Nodes in Pairs
+     * Given a linked list, swap every two adjacent nodes and return its head.
+     * You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs(ListNode head) {
+
+        // base case 退出提交
+        if (head == null || head.next == null) return head;
+        // 获取当前节点的下一个节点
+        ListNode next = head.next;
+        // 进行递归
+        ListNode newNode = swapPairs(next.next);
+        // 这里进行交换
+        next.next = head;
+        head.next = newNode;
+
+        return next;
+    }
+
+    public static ListNode swapPairs2(ListNode head) {
+        ListNode dumyhead = new ListNode(-1); // 设置一个虚拟头结点
+        dumyhead.next = head; // 将虚拟头结点指向head，这样方面后面做删除操作
+        ListNode cur = dumyhead;
+        ListNode temp; // 临时节点，保存两个节点后面的节点
+        ListNode firstnode; // 临时节点，保存两个节点之中的第一个节点
+        ListNode secondnode; // 临时节点，保存两个节点之中的第二个节点
+        while (cur.next != null && cur.next.next != null) {
+            temp = cur.next.next.next;
+            firstnode = cur.next;
+            secondnode = cur.next.next;
+            cur.next = secondnode;       // 步骤一
+            secondnode.next = firstnode; // 步骤二
+            firstnode.next = temp;      // 步骤三
+            cur = firstnode; // cur移动，准备下一轮交换
+        }
+        return dumyhead.next;
+    }
+
+
+    /**
+     * 19. Remove Nth Node From End of List
+     * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+
+        ListNode fastIndex = dummyNode;
+        ListNode slowIndex = dummyNode;
+
+        //只要快慢指针相差 n 个结点即可
+        for (int i = 0; i < n; i++) {
+            fastIndex = fastIndex.next;
+        }
+
+        while (fastIndex.next != null) {
+            fastIndex = fastIndex.next;
+            slowIndex = slowIndex.next;
+        }
+
+        //此时 slowIndex 的位置就是待删除元素的前一个位置。
+        //具体情况可自己画一个链表长度为 3 的图来模拟代码来理解
+        slowIndex.next = slowIndex.next.next;
+        return dummyNode.next;
+    }
+
+    /**
+     * 142. Linked List Cycle II
+     * Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+     * <p>
+     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+     * <p>
+     * Do not modify the linked list.
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {// 有环
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                // 两个指针，从头结点和相遇结点，各走一步，直到相遇，相遇点即为环入口
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 160. Intersection of Two Linked Lists
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != null) { // 求链表A的长度
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null) { // 求链表B的长度
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if (lenB > lenA) {
+            //1. swap (lenA, lenB);
+            int tmpLen = lenA;
+            lenA = lenB;
+            lenB = tmpLen;
+            //2. swap (curA, curB);
+            ListNode tmpNode = curA;
+            curA = curB;
+            curB = tmpNode;
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+
+    }
+
 
 }
--- leetcode/src/test/java/com/kang/linkList/LinkListSolutionTest.java
+++ leetcode/src/test/java/com/kang/linkList/LinkListSolutionTest.java
@@ -47,4 +47,27 @@ public class LinkListSolutionTest {
         node6.next = node7;
         LinkListSolution.reverseList(node1);
     }
+
+    @Test
+    public void swapPairs2() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(5);
+        ListNode node7 = new ListNode(6);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        ListNode result = LinkListSolution.swapPairs(node1);
+        while (result.next != null){
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
 }
