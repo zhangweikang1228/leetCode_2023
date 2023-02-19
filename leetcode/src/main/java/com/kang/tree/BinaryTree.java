1f414b620e0b9acab735c1eb5df6213bb68a3b14
zhangweikang1228 <565927608@qq.com>
2023-02-19 20:14:07 +0000

    leetCode 144.二叉树前序遍历 145.后序遍历 94.中序遍历 102.层次遍历 226.翻转二叉树

---
+++ leetcode/src/main/java/com/kang/tree/BinaryTree.java
@@ -0,0 +1,252 @@
+package com.kang.tree;
+
+import java.util.*;
+
+public class BinaryTree {
+
+
+    /**
+     * 144. Binary Tree Preorder Traversal
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+    private void preorder(TreeNode root, List<Integer> list) {
+        if (null == root) {
+            return;
+        }
+        list.add(root.val);
+        preorder(root.left, list);
+        preorder(root.right, list);
+    }
+
+    public List<Integer> preorderTraversalNoRecursion(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (null != node.right) {
+                stack.push(node.right);
+            }
+            if (null != node.left) {
+                stack.push(node.left);
+            }
+        }
+        ;
+        return list;
+    }
+
+
+    /**
+     * 145. 后序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorder(root, list);
+        return list;
+    }
+
+    private void postorder(TreeNode root, List<Integer> list) {
+        if (null == root) {
+            return;
+        }
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+
+    /**
+     * 后序遍历顺序 左-右-中 入栈顺序：中-左-右 出栈顺序：中-右-左， 最后翻转结果
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversalNoRecursion(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (null != node.left) {
+                stack.push(node.left);
+            }
+            if (null != node.right) {
+                stack.push(node.right);
+            }
+
+
+        }
+        Collections.reverse(list);
+        return list;
+    }
+
+
+    /**
+     * 94.中序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (null == root) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    /**
+     * 中序遍历顺序: 左-中-右 入栈顺序： 左-右
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversalNoRecursion(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()){
+            if (current!=null){
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                list.add(current.val);
+                current = current.right;
+            }
+        }
+
+        return list;
+    }
+
+
+    /**
+     * 102. Binary Tree Level Order Traversal
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> resList = new ArrayList<List<Integer>>();
+        checkFun01(root,0,resList);
+        checkFun02(root,resList);
+        return resList;
+    }
+
+    //DFS--递归方式
+    private void checkFun01(TreeNode node, int deep, List<List<Integer>> resList) {
+        if (node == null) return;
+        deep++;
+
+        if (resList.size() < deep) {
+            //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
+            List<Integer> item = new ArrayList<Integer>();
+            resList.add(item);
+        }
+        resList.get(deep - 1).add(node.val);
+
+        checkFun01(node.left, deep, resList);
+        checkFun01(node.right, deep, resList);
+    }
+
+
+    //BFS--迭代方式--借助队列
+    public void checkFun02(TreeNode node, List<List<Integer>> resList) {
+        if (node == null) return;
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        que.offer(node);
+
+        while (!que.isEmpty()) {
+            List<Integer> itemList = new ArrayList<Integer>();
+            int len = que.size();
+
+            while (len > 0) {
+                TreeNode tmpNode = que.poll();
+                itemList.add(tmpNode.val);
+
+                if (tmpNode.left != null) que.offer(tmpNode.left);
+                if (tmpNode.right != null) que.offer(tmpNode.right);
+                len--;
+            }
+
+            resList.add(itemList);
+        }
+    }
+
+    /**
+     * 226. Invert Binary Tree
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        // 前后序遍历都可以
+        // 中序不行，因为先左孩子交换孩子，再根交换孩子（做完后，右孩子已经变成了原来的左孩子），再右孩子交换孩子（此时其实是对原来的左孩子做交换）
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        swapChildren(root);
+        // 中序
+/*        invertTree(root.left);
+        swapChildren(root);
+        invertTree(root.left);*/
+        return root;
+    }
+
+    private void swapChildren(TreeNode root) {
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+
+    /**
+     * 非递归
+     * @param root
+     * @return
+     */
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {return null;}
+        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            while (size-- > 0) {
+                TreeNode node = deque.poll();
+                swapChildren(node);
+                if (node.left != null) deque.offer(node.left);
+                if (node.right != null) deque.offer(node.right);
+            }
+        }
+        return root;
+    }
+
+
+
+
+}
