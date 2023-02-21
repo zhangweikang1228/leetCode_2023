01590de974e94c9818a3fd3beea9483a2707390c
zhangweikang1228 <565927608@qq.com>
2023-02-21 23:23:26 +0000

    leetCode 222.完全二叉树的节点个数 110.平衡二叉树 257.二叉树的所有路径

--- leetcode/src/main/java/com/kang/tree/BinaryTree.java
+++ leetcode/src/main/java/com/kang/tree/BinaryTree.java
@@ -281,15 +281,16 @@ public class BinaryTree {
             return false;
         }
         // 比较内侧节点
-        boolean inside = compareTwoNodes(left.right,right.left);
+        boolean inside = compareTwoNodes(left.right, right.left);
         // 比较外侧节点
-        boolean outside = compareTwoNodes(left.left,right.right);
+        boolean outside = compareTwoNodes(left.left, right.right);
 
         return inside && outside;
     }
 
     /**
      * 104. Maximum Depth of Binary Tree
+     *
      * @param root
      * @return
      */
@@ -301,6 +302,7 @@ public class BinaryTree {
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
     }
+
     /**
      * 111. Minimum Depth of Binary Tree
      * 递归法，相比求MaxDepth要复杂点
@@ -320,6 +322,156 @@ public class BinaryTree {
         }
         // 左右结点都不为null
         return Math.min(leftDepth, rightDepth) + 1;
+    }
+
+
+    /**
+     * 222. Count Complete Tree Nodes
+     * Given the root of a complete binary tree, return the number of the nodes in the tree.
+     * <p>
+     * According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+     * <p>
+     * Design an algorithm that runs in less than O(n) time complexity.
+     *
+     * @param root
+     * @return
+     */
+    // 通用递归 后序遍历
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = countNodes(root.left);
+        int rightDepth = countNodes(root.right);
+        return leftDepth + rightDepth + 1;
+
+    }
+
+    /**
+     * 针对完全二叉树的解法
+     * <p>
+     * 满二叉树的结点数为：2^depth - 1
+     */
+    public int countNodes2(TreeNode root) {
+        if (root == null) return 0;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        int leftDepth = 0, rightDepth = 0; // 这里初始为0是有目的的，为了下面求指数方便
+        while (left != null) {  // 求左子树深度
+            left = left.left;
+            leftDepth++;
+        }
+        while (right != null) { // 求右子树深度
+            right = right.right;
+            rightDepth++;
+        }
+        if (leftDepth == rightDepth) {
+            return (2 << leftDepth) - 1; // 注意(2<<1) 相当于2^2，所以leftDepth初始为0
+        }
+        return countNodes2(root.left) + countNodes2(root.right) + 1;
+    }
+
+    /**
+     * 110. Balanced Binary Tree
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (null == node) {
+            return 0;
+        }
+        int left = getHeight(node.left);
+        if (-1 == left) {
+            return -1;
+        }
+        int right = getHeight(node.right);
+        if (-1 == right) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    /**
+     * 257. Binary Tree Paths
+     *
+     * @param root
+     * @return
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();// 存最终结果
+        if (null == root) {
+            return res;
+        }
+        List<Integer> paths = new ArrayList<>();// 作为结果中的路径
+        traversal(root, paths, res);
+        return res;
+
+    }
+
+    private void traversal(TreeNode root, List<Integer> paths, List<String> res) {
+        // 用前序遍历
+        paths.add(root.val);
+        if (root.left == null && root.right == null) { //找到了叶子节点
+            // 输出
+            StringBuilder sb = new StringBuilder(); // StringBuilder更快
+            for (int i = 0; i < paths.size()-1; i++) {
+                sb.append(paths.get(i)).append("->");
+            }
+            sb.append(paths.get(paths.size()- 1) );//记录最后一个节点
+            res.add(sb.toString());
+            return;
+        }
+        // 递归和回溯是同时进行的，所以要放在同一个花括号里
+        if (root.left != null) { // 左
+            traversal(root.left, paths, res);
+            paths.remove(paths.size() - 1);// 回溯
+        }
+        if (root.right != null) { // 右
+            traversal(root.right, paths, res);
+            paths.remove(paths.size() - 1);// 回溯
+        }
+
+
+    }
+    /**
+     * 迭代法
+     */
+    public List<String> binaryTreePaths2(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Stack<Object> stack = new Stack<>();
+        // 节点和路径同时入栈
+        stack.push(root);
+        stack.push(root.val + "");
+        while (!stack.isEmpty()) {
+            // 节点和路径同时出栈
+            String path = (String) stack.pop();
+            TreeNode node = (TreeNode) stack.pop();
+            // 若找到叶子节点
+            if (node.left == null && node.right == null) {
+                result.add(path);
+            }
+            //右子节点不为空
+            if (node.right != null) {
+                stack.push(node.right);
+                stack.push(path + "->" + node.right.val);
+            }
+            //左子节点不为空
+            if (node.left != null) {
+                stack.push(node.left);
+                stack.push(path + "->" + node.left.val);
+            }
+        }
+        return result;
     }
 
 
