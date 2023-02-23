4968c2f96cdd1e4c54c94af19fcf0707bbdb00b8
zhangweikang1228 <565927608@qq.com>
2023-02-23 21:49:44 +0000

    leetCode 二叉树：513.找树左下角的值          二叉树：112.路径总和          二叉树：106.构造二叉树          二叉树：654.最大二叉树

--- leetcode/src/main/java/com/kang/tree/BinaryTree.java
+++ leetcode/src/main/java/com/kang/tree/BinaryTree.java
@@ -499,4 +499,119 @@ public class BinaryTree {
     }
 
 
+    /**
+     * 513
+     */
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (i == 0) {
+                    res = poll.val;
+                }
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 112
+     * @param root
+     * @param targetsum
+     * @return
+     */
+    public boolean haspathsum(TreeNode root, int targetsum) {
+        if (root == null) {
+            return false;
+        }
+        targetsum -= root.val;
+        // 叶子结点
+        if (root.left == null && root.right == null) {
+            return targetsum == 0;
+        }
+        if (root.left != null) {
+            boolean left = haspathsum(root.left, targetsum);
+            if (left) {      // 已经找到
+                return true;
+            }
+        }
+        if (root.right != null) {
+            boolean right = haspathsum(root.right, targetsum);
+            if (right) {     // 已经找到
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 106
+     */
+    Map<Integer, Integer> map;  // 方便根据数值查找位置
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) { // 用map保存中序序列的数值对应位置
+            map.put(inorder[i], i);
+        }
+
+        return findNode(inorder,  0, inorder.length, postorder,0, postorder.length);  // 前闭后开
+    }
+
+    private TreeNode findNode(int[] inorder, int inBegin, int inEnd, int[] postorder, int postBegin, int postEnd) {
+        // 参数里的范围都是前闭后开
+        if (inBegin >= inEnd || postBegin >= postEnd) {  // 不满足左闭右开，说明没有元素，返回空树
+            return null;
+        }
+        int rootIndex = map.get(postorder[postEnd - 1]);  // 找到后序遍历的最后一个元素在中序遍历中的位置
+        TreeNode root = new TreeNode(inorder[rootIndex]);  // 构造结点
+        int lenOfLeft = rootIndex - inBegin;  // 保存中序左子树个数，用来确定后序数列的个数
+        root.left = findNode(inorder, inBegin, rootIndex,
+                postorder, postBegin, postBegin + lenOfLeft);
+        root.right = findNode(inorder, rootIndex + 1, inEnd,
+                postorder, postBegin + lenOfLeft, postEnd - 1);
+
+        return root;
+    }
+
+    /**
+     * 654
+     * @param nums
+     * @return
+     */
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree1(nums, 0, nums.length);
+    }
+
+    public TreeNode constructMaximumBinaryTree1(int[] nums, int leftIndex, int rightIndex) {
+        if (rightIndex - leftIndex < 1) {// 没有元素了
+            return null;
+        }
+        if (rightIndex - leftIndex == 1) {// 只有一个元素
+            return new TreeNode(nums[leftIndex]);
+        }
+        int maxIndex = leftIndex;// 最大值所在位置
+        int maxVal = nums[maxIndex];// 最大值
+        for (int i = leftIndex + 1; i < rightIndex; i++) {
+            if (nums[i] > maxVal){
+                maxVal = nums[i];
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxVal);
+        // 根据maxIndex划分左右子树
+        root.left = constructMaximumBinaryTree1(nums, leftIndex, maxIndex);
+        root.right = constructMaximumBinaryTree1(nums, maxIndex + 1, rightIndex);
+        return root;
+    }
+
 }
