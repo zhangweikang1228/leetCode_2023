e5171d9742098b1f5e8087e0a9cc047035b668d3
zhangweikang1228 <565927608@qq.com>
2023-02-25 23:10:05 +0000

    leetCode 617.合并二叉树

--- leetcode/src/main/java/com/kang/tree/BinaryTree.java
+++ leetcode/src/main/java/com/kang/tree/BinaryTree.java
@@ -614,4 +614,44 @@ public class BinaryTree {
         return root;
     }
 
+    /**
+     * 617.合并二叉树
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root2);
+        stack.push(root1);
+        while (!stack.isEmpty()) {
+            TreeNode node1 = stack.pop();
+            TreeNode node2 = stack.pop();
+            node1.val += node2.val;
+            if (node2.right != null && node1.right != null) {
+                stack.push(node2.right);
+                stack.push(node1.right);
+            } else {
+                if (node1.right == null) {
+                    node1.right = node2.right;
+                }
+            }
+            if (node2.left != null && node1.left != null) {
+                stack.push(node2.left);
+                stack.push(node1.left);
+            } else {
+                if (node1.left == null) {
+                    node1.left = node2.left;
+                }
+            }
+        }
+        return root1;
+    }
+
 }
