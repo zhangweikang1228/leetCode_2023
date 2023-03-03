fb2a1546162e5ae0d7b876a067bbfa1dfc3f6721
zhangweikang1228 <565927608@qq.com>
2023-03-03 21:31:16 +0000

    leetCode 贪心算法：455.分发饼干

---
+++ leetcode/src/main/java/com/kang/algorithm/Greedy.java
@@ -0,0 +1,28 @@
+package com.kang.algorithm;
+
+import java.util.Arrays;
+
+public class Greedy {
+
+
+    /**
+     * 455. Assign Cookies
+     * @param g
+     * @param s
+     * @return
+     */
+    // 思路1：优先考虑饼干，小饼干先喂饱小胃口
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < s.length && start < g.length; i++) {
+            if (s[i] >= g[start]) {
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
