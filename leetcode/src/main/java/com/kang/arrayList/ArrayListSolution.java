2d6b25328ce2ce4e5025f3495d330019524a46d9
zhangweikang1228 <565927608@qq.com>
2024-06-18 17:17:21 +0800

    leetCode 2024重启计划！

--- leetcode/src/main/java/com/kang/arrayList/ArrayListSolution.java
+++ leetcode/src/main/java/com/kang/arrayList/ArrayListSolution.java
@@ -43,6 +43,18 @@ public class ArrayListSolution {
      * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
      * If target exists, then return its index. Otherwise, return -1.
      *
+     * Example 1:
+     *
+     * Input: nums = [-1,0,3,5,9,12], target = 9
+     * Output: 4
+     * Explanation: 9 exists in nums and its index is 4
+     * Example 2:
+     *
+     * Input: nums = [-1,0,3,5,9,12], target = 2
+     * Output: -1
+     * Explanation: 2 does not exist in nums so return -1
+     *
+     *
      * @param nums
      * @param target
      * @return
@@ -71,12 +83,27 @@ public class ArrayListSolution {
 
     /**
      * 27. Remove Element
-     * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+     * Given an integer array nums and an integer val, remove all occurrences（出现） of val in nums in-place.
      * The relative order of the elements may be changed.
      * Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums.
      * More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.
      * It does not matter what you leave beyond the first k elements.
      * Return k after placing the final result in the first k slots of nums.
+     *----------------------------------------------------------------------------------------------------------------------------
+     * Example 1:
+     *
+     * Input: nums = [3,2,2,3], val = 3
+     * Output: 2, nums = [2,2,_,_]
+     * Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+     * It does not matter what you leave beyond the returned k (hence they are underscores).
+     * ----------------------------------------------------------------------------------------------------------------------------
+     * Example 2:
+     *
+     * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+     * Output: 5, nums = [0,1,4,0,3,_,_,_]
+     * Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+     * Note that the five elements can be returned in any order.
+     * It does not matter what you leave beyond the returned k (hence they are underscores).
      */
     // 可以用快慢指针，或者 双向指针
     public static int removeElement(int[] nums, int val) {
@@ -119,6 +146,16 @@ public class ArrayListSolution {
      * Given an integer array nums sorted in non-decreasing order,
      * return an array of the squares of each number sorted in non-decreasing order.
      *
+     * Example 1:
+     * Input: nums = [-4,-1,0,3,10]
+     * Output: [0,1,9,16,100]
+     * Explanation: After squaring, the array becomes [16,1,0,9,100].
+     * After sorting, it becomes [0,1,9,16,100].
+     *
+     * Example 2:
+     * Input: nums = [-7,-3,2,3,11]
+     * Output: [4,9,9,49,121]
+     *
      * @param nums
      * @return
      */
@@ -139,10 +176,13 @@ public class ArrayListSolution {
         int j = nums.length - 1;
         while (l <= r) {
             if (nums[l] * nums[l] > nums[r] * nums[r]) {
-                res[j--] = nums[l] * nums[l++];
+                res[j] = nums[l] * nums[l];
+                l++;
             } else {
-                res[j--] = nums[r] * nums[r--];
+                res[j] = nums[r] * nums[r];
+                r--;
             }
+            j--;
         }
         return res;
     }
@@ -151,6 +191,20 @@ public class ArrayListSolution {
      * 209. Minimum Size Subarray Sum
      * Given an array of positive integers nums and a positive integer target, return the minimal length of a
      * subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+     *
+     *
+     * Example 1:
+     * Input: target = 7, nums = [2,3,1,2,4,3]
+     * Output: 2
+     * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+     *
+     * Example 2:
+     * Input: target = 4, nums = [1,4,4]
+     * Output: 1
+     *
+     * Example 3:
+     * Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+     * Output: 0
      *
      * @param target
      * @param nums
@@ -163,11 +217,15 @@ public class ArrayListSolution {
         int result = Integer.MAX_VALUE; // 滑动窗口的长度
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
+            // 注意这里使用while，每次更新 i（起始位置），并不断比较子序列是否符合条件
             while (sum >= target) {
+                // 取子序列的长度
                 result = Math.min(result, right - left + 1); // 从左到右压缩窗口 Compressing the window from left to right
-                sum -= nums[left++];
+                sum = sum - nums[left]; // 这里体现出滑动窗口的精髓之处，不断变更i（子序列的起始位置）
+                left++;
             }
         }
+        // 如果result没有被赋值的话，就返回0，说明没有符合条件的子序列
         return result == Integer.MAX_VALUE ? 0 : result;
 
     }
@@ -175,7 +233,16 @@ public class ArrayListSolution {
     /**
      * 59. Spiral Matrix II
      * Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+     * Example 1:
+     *    ｜1｜2｜3｜
+     *    ｜8｜9｜4｜
+     *    ｜7｜6｜5｜
+     * Input: n = 3
+     * Output: [[1,2,3],[8,9,4],[7,6,5]]
+     * Example 2:
      *
+     * Input: n = 1
+     * Output: [[1]]
      * @param n
      * @return
      */
@@ -190,7 +257,11 @@ public class ArrayListSolution {
         // 填充值
         int count = 1;
         int i, j;
+        // loop = n / 2 每个圈循环几次，例如n为奇数3，那么loop = 1 只是循环一圈，矩阵中间的值需要单独处理
         while (loop++ < n / 2) { // 判断循环几圈，loop从1开始
+
+            // 下面开始的四个for就是模拟转了一圈
+
             // 上面：从左到右
             for (j = start; j < n - loop; j++) { // 左闭右开，每循环一次，右边就少一个不用填
                 result[start][j] = count++;
@@ -209,6 +280,8 @@ public class ArrayListSolution {
             }
             start++;
         }
+
+        //  矩阵中间的位置，例如：n为3， 中间的位置就是(1，1)，n为5，中间位置为(2, 2)
         if (n % 2 == 1) {
             result[start][start] = n * n;
         }
